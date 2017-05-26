cec7ec1df8d42e000d7797b102c90fafd3d677b6
ajinkyapadwad <ajinkyapadwad@gmail.com>
2017-05-26 14:37:58 -0400

    new

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,11 @@
 <?php include 'includes\header.php' ?>
-
+<?php 
+$db = new Database;
+$query = "SELECT posts.*, categories.name FROM posts
+            INNER JOIN  categories
+            ON posts.category = categories.id";
+$posts = $db->select($query);
+?>
 <table class="table table-striped">
 
 <tr>
@@ -9,13 +15,17 @@
     <th>AUTHOR</th>
     <TH>DATE</TH>
 </tr>
+
+<?php while($row = $posts -> fetch_assoc()) : ?>
 <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
+    <td><?php echo $row['id']; ?></td>
+    <td><a href="edit_post.php?id=<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a></td>
+    <td><?php echo $row['category']; ?></td>
+    <td><?php echo $row['author']; ?></td>
+    <td><?php echo Fdate($row['time']); ?></td>
 </tr>
+<?php endwhile; ?>
+
 
 <tr>
     <th>ID</th>
